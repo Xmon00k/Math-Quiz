8a7687d597bb08c15a37246aeec9da1a24f4c7bd
Xmon00k <142735854+Xmon00k@users.noreply.github.com>
2023-10-18 18:51:27 -0400

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -5,5 +5,5 @@ function addUser() {
     localStorage.setItem("player1_name", player1_name);
     localStorage.setItem("player2_name", player2_name);
 
-    window.location = "game_page.html";
+    window.location = "quiz_game_page.html";
 }
